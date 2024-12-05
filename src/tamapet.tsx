2561b0b3ced72ce747e501e2d6ead0d6bba85894
Adrian Segura <adrianlsegura@gmail.com>
2024-12-04 17:57:16 -0800

    New tamapet logic code WIP.

--- src/tamapet.tsx
+++ src/tamapet.tsx
@@ -1,10 +1,33 @@
-type Tamapet = {
+import React from "react";
+
+type Action = "feed" | "play" | "sleep" | "check";
+
+interface TamaInformation {
   name: string;
-  species: string;
-  age: number;
-  happiness: number;
   fullness: number;
+  happiness: number;
   energy: number;
+  species: string;
+  status: string;
+  age: number;
+  isAlive: boolean;
+}
+
+const species = {
+  egg: {
+    name: "Egg",
+    fullness: 100,
+    happiness: 100,
+    energy: 100,
+  },
+  TESTSPECIES: {
+    name: "TEST",
+    fullness: 50,
+    happiness: 50,
+    energy: 50,
+  }
 };
 
-function createPet(name: string) {}
+const Tamapet: React.FC = () => {
+  
+}
