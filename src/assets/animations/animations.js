909e90666fb4ac1eee6601cccf91b900f185daee
Maissakk <maissakobelekeita@gmail.com>
2024-12-05 18:27:10 -0500

    animation

--- src/assets/animations/animations.js
+++ src/assets/animations/animations.js
@@ -1,21 +1,21 @@
 const animations = {
     mochi: {
         default: "/src/assets/animations/kittyanim/mochi_default.png",
-        eating: "/src/assets/animations/kittyanim/mochi_eat.gif",
-        playing: "/src/assets/animations/kittyanim/mochi_play.gif",
-        sleep: "/src/assets/animations/kittyanim/mochi_sleep.gif",
+        eating: "/src/assets/animations/kittyanim/mochi_eat.png",
+        playing: "/src/assets/animations/kittyanim/mochi_play.png",
+        sleep: "/src/assets/animations/kittyanim/mochi_sleep.png",
     },
     kitty: { 
       default: "/src/assets/animations/kittyanim/kitty_default.png",
-      eating: "/src/assets/animations/kittyanim/kitty_eat.gif",
-      playing: "/src/assets/animations/kittyanim/kitty_play.gif",
-      sleep: "/src/assets/animations/kittyanim/kitty_sleep.gif",
+      eating: "/src/assets/animations/kittyanim/kitty_eat.png",
+      playing: "/src/assets/animations/kittyanim/kitty_play.png",
+      sleep: "/src/assets/animations/kittyanim/kitty_sleep.png",
     },
     whale: {
         default: "/src/assets/animations/whaleanim/whale_default.png",
-        eating: "/src/assets/animations/whaleanim/whale_eat.gif",
-        playing: "/src/assets/animations/whaleanim/whale_play.gif",
-        sleep: "/src/assets/animations/whaleanim/whale_sleep.gif",
+        eating: "/src/assets/animations/whaleanim/whale_eat.png",
+        playing: "/src/assets/animations/whaleanim/whale_play.png",
+        sleep: "/src/assets/animations/whaleanim/whale_sleep.png",
     },
   };
   
