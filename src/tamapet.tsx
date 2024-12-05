af9e4e6c35e13a022b354bec2e35721c94ed91c3
Adrian Segura <adrianlsegura@gmail.com>
2024-12-04 18:13:25 -0800

    Added all species.

--- src/tamapet.tsx
+++ src/tamapet.tsx
@@ -18,16 +18,34 @@ const species = {
     name: "Egg",
     fullness: 100,
     happiness: 100,
-    energy: 100,
+    energy: 100
   },
-  TESTSPECIES: {
-    name: "TEST",
-    fullness: 50,
-    happiness: 50,
-    energy: 50,
+  byebyekitty: {
+    name: "Byebye Kitty",
+    fullness: 30,
+    happiness: 30,
+    energy: 50
+  },
+  whale: {
+    Name: "Whale",
+    fullness: 10,
+    happiness: 60,
+    energy: 20
+  },
+  mochi: {
+    Name: "Mochi",
+    fullness: 90,
+    happiness: 80,
+    energy: 90
+  },
+  peach: {
+    Name: "BBL",
+    fullness: 100,
+    happiness: 100,
+    energy: 10
   }
 };
 
 const Tamapet: React.FC = () => {
-  
+
 }
