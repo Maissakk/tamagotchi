a523f343bac66958b0735e08d92099f475e5e9a9
Adrian Segura <adrianlsegura@gmail.com>
2024-12-05 05:38:30 -0800

    Updated tamapet.tsx to decrement values overtime.

--- src/tamapet.tsx
+++ src/tamapet.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const TIME: number = 2000; // 2 Seconds
+
+interface TamaProps {
+  name: string;
+}
 
 type Action = "feed" | "play" | "sleep" | "check";
 
-interface TamaInformation {
-  name: string;
+interface TamaState {
   fullness: number;
   happiness: number;
   energy: number;
@@ -16,7 +21,7 @@ const species = {
     happiness: 0,
     energy: 0
   },
-  byebyekitty: {
+  kitty: {
     fullness: 30,
     happiness: 30,
     energy: 50
@@ -33,15 +38,36 @@ const species = {
   }
 };
 
-const initialState: TamaInformation = {
-  // Find way to input name
-  name: "Tamapet",
+const initialState: TamaState = {
   fullness: species.egg.fullness,
   happiness: species.egg.happiness,
   energy: species.egg.energy,
   species: "egg"
 }
 
-const Tamapet: React.FC = () => {
+const Tamapet: React.FC<TamaProps> = ({ name }) => {
+  const [state, setState] = useState<TamaState>(initialState);
+
+  // This handles the pet state by decrementing all stats by the set TIME.
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState((prevState) => {
+        const newFullness = Math.max(prevState.fullness - 1, 0);
+        const newHappiness = Math.max(prevState.happiness - 1, 0);
+        const newEnergy = Math.max(prevState.energy - 1, 0);
+
+        return {
+          ...prevState,
+          fullness: newFullness,
+          happiness: newHappiness,
+          energy: newEnergy
+        };
+      });
+    }, TIME);
+
+    return () => clearInterval(interval);
+  }, []);
 
 }
+
+export default Tamapet;
